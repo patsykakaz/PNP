d2fe0ff47099106ffabd59587ffee895a0babae7
patsykakaz_LSC <patsykakaz_LSC@MacBook-Pro-de-patsykakaz-LSC.local>
2015-09-04 14:47:21 +0200

    improved navbar a bit

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,38 @@
 $(document).ready(function(){
 
-    $(window).load(function(){
-        $;
-    });
 
+    //
+    // parent = $('.navbar-collapse .container');
+    // parentW = parent.width();
+    // catDrop = $('.dropdown-category');
+    // highlightsDrop = $('.dropdown-highlights');
+    // logoDrop = $('.dropdown-logo');
+
+    // catDrop.width($('.navbar-brand').outerWidth());
+    // if($(window).width() > 768){
+    //     logoDrop.width(parentW*0.3);
+    // }else{
+    //     logoDrop.css('display','none');
+    // }
+    // highlightsDrop.width(parentW*0.7-(logoDrop.outerWidth()+5));
+    // highlightsDrop.css('margin-left','-5px');
+    Navbar();
 });
+
+$(window).resize(function(){
+    Navbar();
+});
+
+function Navbar(){
+    if($(window).width() < 992){
+        $('.highlight-box:eq(2)').addClass('hide');
+    }else{
+        $('.highlight-box:eq(2)').removeClass('hide');
+    }
+
+    $("*").each(function(){
+        if($(this).outerWidth()>$(window).width()){
+            // alert($(this).attr('class'));
+        }
+    });
+}
